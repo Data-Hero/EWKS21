df7dd403211069c9d7118466339a8d57b8977ce1
Max Martens <mmartens@Maxs-MBP.fritz.box>
2021-05-19 14:06:08 +0200

    Fix package name + Setup embedded h2 db

--- isomatic-device/src/main/java/de/joergiso/isomatic/device/IsomaticDeviceApplication.java
+++ isomatic-device/src/main/java/de/joergiso/isomatic/device/IsomaticDeviceApplication.java
@@ -1,4 +1,4 @@
-package de.joergiso.isomaticdevice;
+package de.joergiso.isomatic.device;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
