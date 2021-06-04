9554593a9c22b666730cc1a3ad251f9187dd14de
Max Martens <max.martens012@stud.fh-dortmund.de>
2021-06-04 18:08:04 +0200

    config: init config server

---
+++ isomatic-config-server/src/test/java/de/joergiso/isomatic/configserver/IsomaticConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.joergiso.isomatic.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsomaticConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
