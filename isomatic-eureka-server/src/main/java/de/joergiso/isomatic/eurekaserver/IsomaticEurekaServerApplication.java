02e11aabe1c6015c08d0ca512ea96fc39cb7e396
Max Martens <max.martens012@stud.fh-dortmund.de>
2021-06-04 16:55:05 +0200

    eureka: initialize

---
+++ isomatic-eureka-server/src/main/java/de/joergiso/isomatic/eurekaserver/IsomaticEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package de.joergiso.isomatic.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsomaticEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsomaticEurekaServerApplication.class, args);
+	}
+
+}
