ddf65638c356a18a3ea3fa516172af86ced185be
Max Martens <max.martens012@stud.fh-dortmund.de>
2021-06-04 17:52:28 +0200

    device: implement eureka client

--- isomatic-device/src/main/java/de/joergiso/isomatic/device/IsomaticDeviceApplication.java
+++ isomatic-device/src/main/java/de/joergiso/isomatic/device/IsomaticDeviceApplication.java
@@ -2,8 +2,10 @@ package de.joergiso.isomatic.device;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class IsomaticDeviceApplication {
 
 	public static void main(String[] args) {
