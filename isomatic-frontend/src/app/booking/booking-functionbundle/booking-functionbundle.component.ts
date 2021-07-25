b84e4a6707e073cccd5f633691402c013211ded0
riese <b.riesenberg@gmx.de>
2021-07-25 23:54:45 +0200

    booking: frontend add function bundle

--- isomatic-frontend/src/app/booking/booking-functionbundle/booking-functionbundle.component.ts
+++ isomatic-frontend/src/app/booking/booking-functionbundle/booking-functionbundle.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import {BookingControllerService} from '../booking-services/booking-controller.service';
 import {MatTableDataSource} from '@angular/material/table';
 
@@ -11,12 +11,12 @@ export interface FunctionBundle {
 }
 
 const ELEMENT_DATA: FunctionBundle[] = [
-  { functionBundleId: 'FB1', functionNumber: 'F1', priceByMinute: 1.0079, discount: 0.1 },
-  { functionBundleId: 'FB2', functionNumber: 'F2', priceByMinute: 4.0026, discount: 0.1 },
-  { functionBundleId: 'FB3', functionNumber: 'F3', priceByMinute: 6.941,  discount: 0.1 },
-  { functionBundleId: 'FB4', functionNumber: 'F4', priceByMinute: 9.0122, discount: 0.1 },
-  { functionBundleId: 'FB5', functionNumber: 'F5', priceByMinute: 10.811, discount: 0.1 },
-  { functionBundleId: 'FB6', functionNumber: 'F6', priceByMinute: 12.0107,discount: 0.1 }
+  {functionBundleId: 'FB1', functionNumber: 'F1', priceByMinute: 1.0079, discount: 0.1},
+  {functionBundleId: 'FB2', functionNumber: 'F2', priceByMinute: 4.0026, discount: 0.1},
+  {functionBundleId: 'FB3', functionNumber: 'F3', priceByMinute: 6.941, discount: 0.1},
+  {functionBundleId: 'FB4', functionNumber: 'F4', priceByMinute: 9.0122, discount: 0.1},
+  {functionBundleId: 'FB5', functionNumber: 'F5', priceByMinute: 10.811, discount: 0.1},
+  {functionBundleId: 'FB6', functionNumber: 'F6', priceByMinute: 12.0107, discount: 0.1}
 ];
 
 
@@ -30,12 +30,20 @@ export class BookingFunctionbundleComponent implements OnInit {
   displayedColumns: string[] = ['functionBundleId', 'functionNumber', 'priceByMinute', 'discount'];
   dataSource: MatTableDataSource<FunctionBundle> = new MatTableDataSource(ELEMENT_DATA);
   uploadForm: FormGroup;
+  addForm: FormGroup;
+  addFunctionBundleForm: boolean = false;
 
   constructor(private bookingControllerService: BookingControllerService,
               private changeDetectorRefs: ChangeDetectorRef) {
     this.uploadForm = new FormGroup({
       UserId: new FormControl()
-    })
+    });
+    this.addForm = new FormGroup({
+      FunctionBundleId: new FormControl(),
+      FunctionId: new FormControl(),
+      Discount: new FormControl(),
+      PriceByMinute: new FormControl()
+    });
   }
 
   ngOnInit(): void {
@@ -46,21 +54,21 @@ export class BookingFunctionbundleComponent implements OnInit {
     // @ts-ignore
     formData.append('UserId', this.uploadForm.get('UserId').value);
     // @ts-ignore
-    console.log(this.dataSource)
+    console.log(this.dataSource);
     // @ts-ignore
-    this.bookingControllerService.getAvailableFunctionBundles(+formData.get('UserId').valueOf(), "body")
+    this.bookingControllerService.getAvailableFunctionBundles(+formData.get('UserId').valueOf(), 'body')
       .subscribe(value => {
         let response: FunctionBundle[] = [];
         value.forEach(element => {
           console.log(element.functionNumber);
           response.push({
-            functionBundleId: element.functionBundleId!,
-            functionNumber: element.functionNumber?.pop()!,
-            discount: element.discount!,
-            priceByMinute: element.priceByMinute!
-          }
-          )
-        })
+              functionBundleId: element.functionBundleId!,
+              functionNumber: element.functionNumber?.pop()!,
+              discount: element.discount!,
+              priceByMinute: element.priceByMinute!
+            }
+          );
+        });
         this.dataSource.data = response;
       });
     this.changeDetectorRefs.detectChanges();
@@ -68,5 +76,28 @@ export class BookingFunctionbundleComponent implements OnInit {
 
   onTextChange($event: Event) {
 
+  }
+
+  showAddFunctionBundleForm() {
+    this.addFunctionBundleForm = true;
+  }
+
+  hideAddFunctionBundleForm() {
+    this.addFunctionBundleForm = false;
+  }
+
+  onCreate() {
+    const formData = new FormData();
+    this.bookingControllerService.addFunctionBundle({
+      // @ts-ignore
+      functionBundleId: this.addForm.get('FunctionBundleId').value,
+      // @ts-ignore
+      functionNumber:  this.addForm.get('FunctionId').value.split(","),
+      // @ts-ignore
+      discount:  this.addForm.get('Discount').value,
+      // @ts-ignore
+      priceByMinute:  this.addForm.get('PriceByMinute').value
+      }
+    ).subscribe(value => console.log(value));
   }
 }
