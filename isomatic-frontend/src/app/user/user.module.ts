fac5a4127512dd34e12cc373bb96ae439f46a811
Thomas Schwarz <thomas.schwarz026@stud.fh-dortmund.de>
2021-07-28 19:11:59 +0200

    user-frontend: remove unnecessary code

--- isomatic-frontend/src/app/user/user.module.ts
+++ isomatic-frontend/src/app/user/user.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
-import { AddDeviceComponent } from './add-device/add-device.component';
 import { UserListComponent } from './user-list/user-list.component';
 import {FormsModule} from "@angular/forms";
 
@@ -10,7 +9,6 @@ import {FormsModule} from "@angular/forms";
 @NgModule({
   declarations: [
     UserComponent,
-    AddDeviceComponent,
     UserListComponent
   ],
   imports: [
