9ac16b804f0ba3b4d5bef887771928199329af26
riese <b.riesenberg@gmx.de>
2021-07-28 19:50:15 +0200

    delete unused resttemplate

--- isomatic-booking/src/main/java/de/joergiso/isomaticbooking/repository/RemoteUserRepository.java
+++ isomatic-booking/src/main/java/de/joergiso/isomaticbooking/repository/RemoteUserRepository.java
@@ -35,15 +35,6 @@ public class RemoteUserRepository {
   }
 
   public User fetchUser(Long userId) throws UserNotFoundException {
-    ResponseEntity<String> response2
-        = restTemplate.exchange(
-        configurationService.getUserEndpoint()
-            + "/user/"
-            + userId,
-        HttpMethod.GET,
-        null,
-        new ParameterizedTypeReference<String>() { });
-    System.out.println("remote user call" + response2);
     Optional<User> user = circuitBreakerFactory.create("fetchUser").run(() -> {
       ResponseEntity<User> response
           = restTemplate.exchange(
@@ -56,7 +47,6 @@ public class RemoteUserRepository {
       );
       return Optional.ofNullable(response.getBody());
     }, t -> userRepository.findById(userId));
-    System.out.println(user);
     user.ifPresent(userRepository::save);
     return user.orElseThrow(UserNotFoundException::new);
   }
